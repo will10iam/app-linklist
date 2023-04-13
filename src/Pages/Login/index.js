f2c1188b9e66806250871e3cd01fd250f3a8dfe2
William Berbet <wiiliamb@hotmail.com>
2023-04-13 07:59:40 -0300

    pagina login + primeiros hooks

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './login.css'
+import Logo from '../../Components/Logo'
 
 export default function Login() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    function handleLogin(e) {
+        e.preventDefault()
+
+        console.log(email)
+        console.log(password)
+    }
+
+
     return (
-        <div>Login</div>
+        <div className='login-container'>
+            <Logo />
+
+            <form className='form' onSubmit={handleLogin}>
+                <input
+                    type='email'
+                    placeholder='Digite seu email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+
+                <input
+                    type='password'
+                    placeholder='*********'
+                    autoComplete='on'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+
+                <button type='submit'>Acessar</button>
+            </form>
+        </div>
     )
 }
 
