1d4070de8b14e04927dcdc94bd726ce0c8578202
William Berbet <wiiliamb@hotmail.com>
2023-04-17 21:20:00 -0300

    componente header

---
+++ src/Components/Header/index.js
@@ -0,0 +1,34 @@
+import './header.css'
+import { AiOutlinePoweroff } from 'react-icons/ai'
+import { auth } from '../../Services/firebaseConnection'
+import { signOut } from "firebase/auth"
+
+import React from 'react'
+
+export default function Header() {
+    const toggleMenu = () =>
+        document.body.classList.toggle("open");
+
+
+    async function handleLogout() {
+        await signOut(auth);
+    }
+
+
+    return (
+        <div className='container'>
+            <button className='burger' onClick={toggleMenu}>
+                <div className='background'></div>
+                <div className='menu'>
+                    <nav>
+                        <a>Links</a>
+                        <a>Redes Sociais</a>
+                        <a href={handleLogout} onClick={handleLogout}><AiOutlinePoweroff /></a>
+                    </nav>
+                </div>
+            </button>
+
+
+        </div>
+    )
+}
--- src/Pages/Admin/index.js
+++ src/Pages/Admin/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import Header from '../../Components/Header'
 
 export default function Admin() {
     return (
-        <div>Admin</div>
+        <Header />
     )
 }
